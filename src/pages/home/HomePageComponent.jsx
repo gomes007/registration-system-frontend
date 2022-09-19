cf54a92fb8ed50d38907ed355561379039f33cfc
Douglas Fernandes <douglasf.filho@gmail.com>
2022-09-19 20:23:38 -0300

    add: exibir total de vendas

--- src/pages/home/HomePageComponent.jsx
+++ src/pages/home/HomePageComponent.jsx
@@ -4,17 +4,38 @@ import SalesByDayGraph from "./SalesByDayGraph";
 import SalesGraph from "./SalesGraph";
 
 export default function HomePageComponent(props) {
-
     const [list, setList] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [totalValue, setTotalValue] = useState(0);
 
     useEffect(() => {
         getAllSalesWebSocket(setList);
     }, []);
 
+    useEffect(() => {
+        setTotal(list.length);
+        const salesSubstract = list.reduce(
+            (p, c) => {
+                return p + c.total;
+            },
+            0
+        ).toFixed(2);
+        setTotalValue(salesSubstract);
+    }, [list]);
+
     return (
         <div className="container content">
-            <SalesGraph data={list}/>
+            {/* <SalesGraph data={list}/> */}
             <SalesByDayGraph data={list} />
+            <div className="sales-total">
+                <p>Total de vendas:</p>
+                <p>{total}</p>
+            </div>
+
+            <div className="sales-total">
+                <p>Valor total de vendas:</p>
+                <p>R$ {totalValue}</p>
+            </div>
         </div>
     )
 }
